b9543533f915e1d582eb3980f3e8e68cca346c55
nino <ninosnoek@gmail.com>
2021-04-26 12:06:15 +0200

    readlogs

--- Website/python/Change PSW user.py
+++ Website/python/Change PSW user.py
@@ -1,9 +1,10 @@
 import pymysql
+import fundementals
 
 conn = pymysql.connect(
-    db=fundementals.database,
-    user=fundementals.username,
-    passwd=fundementals.password,
+    db=fundementals.db_database,
+    user=fundementals.db_user,
+    passwd=fundementals.db_password,
     host='localhost',
     port=3306)
 c = conn.cursor()
