6be839bac37f261f3a87142d76747d612c649926
menno <menno@DESKTOP-MK1MBQO>
2021-04-28 15:51:20 +0200

    V

--- Website/python/Change PSW User.php
+++ Website/python/Change PSW User.php
@@ -11,4 +11,41 @@ if (!$conn) {
 }
 echo "Connected successfully";
 
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Capture the flag</title>
+        <meta name="description" content="Capture the flag">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="ChangePSWPHP.css">
+    </head>
+    <body>
+        <div class="grid-container">
+            <div class="nav-bar">
+                <span id="left">IC101-C5</span>
+                <span id="right">Nino, Menno, Naoufal, Thomas, Tirez</span>
+            </div>
+            <div class="Main-page">
+                $result = mysqli_query(con,"SELECT * FROM firstName");
+                echo "<table border='1'>
+                <tr>
+                <th>firstName</th>
+                </tr>";
+
+                while($row = mysqli_fetch_array($result))
+                {
+                echo "<tr>";
+                echo "<td>" . $row['firstName'] . "</td>";
+                echo "</tr>";
+                }
+                echo "</table>";
+
+            </div>
+        </div>
+    </body>
+</html>
+
+mysqli_close($con);
 ?>
