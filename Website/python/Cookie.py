ec8207783c6b6e100ad14e7945ddbf7c67e8302f
nino <ninosnoek@gmail.com>
2021-05-19 14:32:49 +0200

    cookie

---
+++ Website/python/Cookie.py
@@ -0,0 +1,8 @@
+#adminindex gelocked door cookie?
+#Als isAdmin = True unlock pagina else locked
+#adminindex mag alleen worden gezien door de isAdmin cookies true
+#als een gebruiker de pagina bezoekt maak cookie aan en kijk welke waarde de cookie heeft en verander
+if isAdmin = True
+    /adminindex.html
+else :
+    print("error 404")
