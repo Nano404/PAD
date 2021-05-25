60365523c10ac35758bc46a346a26835bbed421a
nino <ninosnoek@gmail.com>
2021-05-25 21:02:07 +0200

    commit

--- py/ipban.py
+++ py/ipban.py
@@ -2,8 +2,39 @@
 # Print necessary headers.
 print("Content-Type: text/html")
 print()
+import pymysql
 import cgitb
 import cgi
 import os
+import sys
 cgitb.enable()
-ip = cgi.escape(os.environ["REMOTE_ADDR"])
+
+conn = pymysql.connect(
+    db="pad",
+    user="pad",
+    passwd="Cybersec2021",
+    host='localhost',
+    port=3306)
+c = conn.cursor()
+
+form = cgi.FieldStorage()
+ip = form.getvalue("ip")
+
+select = "SELECT * FROM `ipban` WHERE `ip` = " + ip
+c.execute(select)
+conn.commit()
+
+if c.fetchone():
+    print("ip banned")
+else:
+    sql = "INSERT INTO 'ipban' (`ip`) VALUES (%s)"
+    val = ip
+    c.execute(sql, val)
+    conn.commit()
+    print("ip banned")
+
+
+
+
+
+
