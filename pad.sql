d6884c78953eebab8ac511531d76f52a6e476758
Thomas Verhoeven <thomas.verhoeven3@hva.nl>
2021-05-26 11:06:12 +0000

    PAD sql

---
+++ pad.sql
@@ -0,0 +1,141 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 26, 2021 at 11:05 AM
+-- Server version: 5.7.31
+-- PHP Version: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pad`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `form_requests`
+--
+
+DROP TABLE IF EXISTS `form_requests`;
+CREATE TABLE IF NOT EXISTS `form_requests` (
+  `idform_requests` int(11) NOT NULL AUTO_INCREMENT,
+  `form_comment` mediumtext,
+  `form_email` varchar(45) DEFAULT NULL,
+  `form_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idform_requests`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ip`
+--
+
+DROP TABLE IF EXISTS `ip`;
+CREATE TABLE IF NOT EXISTS `ip` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+CREATE TABLE IF NOT EXISTS `login` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_login_idx` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `username`, `password`, `user_id`) VALUES
+(1, 'admin', 'password', 2),
+(2, 'henk', '123', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rollen`
+--
+
+DROP TABLE IF EXISTS `rollen`;
+CREATE TABLE IF NOT EXISTS `rollen` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `rol_naam` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_rol_idx` (`rol_naam`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `rollen`
+--
+
+INSERT INTO `rollen` (`id`, `rol_naam`) VALUES
+(2, 'admin'),
+(1, 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `rol` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_rol_idx` (`rol`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `rol`, `email`) VALUES
+(1, 'user', 'henk@gmail.com'),
+(2, 'admin', 'admin@ctf.nl'),
+(3, 'user', 'test@test.nl');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `login`
+--
+ALTER TABLE `login`
+  ADD CONSTRAINT `user_login` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_rol` FOREIGN KEY (`rol`) REFERENCES `rollen` (`rol_naam`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
