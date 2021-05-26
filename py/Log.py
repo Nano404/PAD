7cba1a67e5ad5e1e47937da944211552b7eaac4e
nino <ninosnoek@gmail.com>
2021-05-26 14:07:34 +0200

    log

--- py/Log.py
+++ py/Log.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+import cgitb
+cgitb.enable()
+# Print necessary headers.
+print("Content-Type: text/html")
+print()
+import cgitb
 import os
 read_logs = "cat /var/log/apache2/access.log"
 log_result = os.system(read_logs)
