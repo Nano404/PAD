8f9f4dbafc0c959e6dbf1cf6cce750c6d6ed332e
nino <ninosnoek@gmail.com>
2021-05-26 13:32:24 +0200

    minor changes

--- py/form_request.py
+++ py/form_request.py
@@ -7,12 +7,13 @@ print()
 import pymysql
 import cgitb
 import cgi
-
+#Haalt gegevens op uit get request
 form = cgi.FieldStorage()
 comment = form.getvalue("comment")
 name = form.getvalue("username")
 email = form.getvalue("email")
 
+#connect met database
 conn = pymysql.connect(
     db="pad",
     user="pad",
@@ -20,7 +21,7 @@ conn = pymysql.connect(
     host='db',)
 c = conn.cursor()
 
-
+#insert de gegevens in de aangegeven database
 insert = """INSERT INTO `pad`.`form_requests` (`idform_requests`, `form_comment`, `form_email`, `form_name`) VALUES (0, %s, %s, %s);"""
 insert_val = (comment, email, name)
 c.execute(insert, insert_val)
@@ -29,6 +30,5 @@ conn.commit()
 select = "SELECT * FROM `form_requests` WHERE `form_name`=%s"
 c.execute(select, (name))
 results = c.fetchall()
-#print("The following message has been sent {}".format(c.fetchall()))
 for r in results:
-    print(r)
+    print(r"\n")
--- py/reload.py
+++ py/reload.py
@@ -3,6 +3,7 @@ print("Content-type: text/html")
 print()
 import cgitb; cgitb.enable(display=1, logdir=None, context=5, format="html")
 import os
+#spreek voor zich reload de apache site
 restart = "/etc/init.d/apache2 reload > /dev/null"
 os.system(restart)
 print("Apache reloading")
